409ffccf6439eefa8a9e8bee99a13d4ef72671a4
Bruno Alves <alves.bruno945@gmail.com>
2024-02-08 20:50:15 -0300

    Add  password

--- KidsApp/LoginView.swift
+++ KidsApp/LoginView.swift
@@ -1,29 +1,35 @@
 import SwiftUI
 
 struct LoginView: View {
-
+    
     @State private var textFieldText: String = ""
     @State private var passwordText: String = ""
     @State private var disableButton: Bool = true
-
+    
     var body: some View {
-
+        
         NavigationView {
-
+            
             VStack {
                 TextField("username",
                           text: $textFieldText)
-                    .padding()
-                    .background(Color.gray.opacity(0.3).clipShape(.buttonBorder))
-                    .font(.headline)
+                .padding()
+                .background(Color.gray.opacity(0.3).clipShape(.buttonBorder))
+                .font(.headline)
+                
+                TextField("Password",
+                          text: $passwordText)
+                .padding()
+                .background(Color.gray.opacity(0.3).clipShape(.buttonBorder))
+                .font(.headline)
             }
             .padding()
             .navigationTitle("Login")
         }
     }
-
+    
     // MARK: - Private Methods
-
+    
     private func loginButton() -> Bool {
         return textFieldText.isEmpty && passwordText.isEmpty
     }
