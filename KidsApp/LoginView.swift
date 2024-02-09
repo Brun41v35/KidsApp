572363474a1e3164e4bc92e9fd0b1857f0ed2a21
Bruno Alves <alves.bruno945@gmail.com>
2024-02-08 23:18:32 -0300

    Enabling button when text field has value

--- KidsApp/LoginView.swift
+++ KidsApp/LoginView.swift
@@ -1,30 +1,34 @@
 import SwiftUI
 
 struct LoginView: View {
-    
+
+    // MARK: - Private Properties
+
     @State private var textFieldText: String = ""
     @State private var passwordText: String = ""
     @State private var disableButton: Bool = true
-    
+
+    // MARK: - Body
+
     var body: some View {
-        
+
         NavigationView {
-            
+
             VStack {
                 TextField("Username",
                           text: $textFieldText)
                 .padding()
                 .background(Color.gray.opacity(0.3).clipShape(.buttonBorder))
                 .font(.headline)
-                
+
                 TextField("Password",
                           text: $passwordText)
                 .padding()
                 .background(Color.gray.opacity(0.3).clipShape(.buttonBorder))
                 .font(.headline)
-                
+
                 Button(action: {
-                    
+                    login()
                 }, label: {
                     Text("login".uppercased())
                         .padding()
@@ -34,19 +38,25 @@ struct LoginView: View {
                         .foregroundStyle(.white)
                         .font(.headline)
                 })
-                .disabled(disableButton)
+                .disabled(textFieldHasValue())
             }
             .padding()
             .navigationTitle("Login")
         }
     }
-    
+
     // MARK: - Private Methods
-    
+
     private func textFieldHasValue() -> Bool {
         return textFieldText.isEmpty || passwordText.isEmpty
     }
+
+    private func login() {
+        print("Go to list view..")
+    }
 }
+
+// MARK: - Preview
 
 #Preview {
     LoginView()
