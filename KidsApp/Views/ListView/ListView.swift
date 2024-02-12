26e72f624ad8b54c88ab560d9896ae8f5b137e68
Bruno Alves <alves.bruno945@gmail.com>
2024-02-11 22:36:04 -0300

    Add background color at `ListView`

--- KidsApp/Views/ListView/ListView.swift
+++ KidsApp/Views/ListView/ListView.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 
 struct ListView: View {
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        ZStack {
+            Color.yellow.opacity(0.3)
+                .ignoresSafeArea()
+        }
     }
 }
 
